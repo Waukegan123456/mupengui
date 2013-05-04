542f883cd067f9b6710187fc318ee0a3304056d7
dell <dell@dell-comp>
2013-05-04 18:44:10 +0545

    Built new debian binary

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 rm *.build *.changes *.diff.gz *.dsc
 mv mupengui-0.1/debian .
 rm -rf mupengui-0.1/obj-x86_64-linux-gnu
+rm -rf mupengui-0.1/debian/mupengui
+rm mupengui-0.1/mupengui
+rm mupengui-0.1/Makefile
 tar -czf mupengui_0.1.orig.tar.gz mupengui-0.1
 mv debian mupengui-0.1
 cd mupengui-0.1
